ab71cb65a4706e3fd0f10820b2ba969be48f47a6
DominikHess <73423350+DominikHess@users.noreply.github.com>
2022-01-18 14:53:31 +0100

    Add fonts, design main menu screen, add sound effect

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,16 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
+    private AudioSource audio;
+
     void Start()
     {
+        audio = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        if (SceneManager.GetActiveScene().name == "01_MainMenu" && Input.anyKey)
+        {
+            audio.Play();
+            Invoke("StartGame", audio.clip.length);
+        }
     }
 
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("01_MainMenu");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("02_Game");
     }
 }
